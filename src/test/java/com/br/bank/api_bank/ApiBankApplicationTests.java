4f5972f58cb7e0fb49648e01d3b490ccbb0c4685
Joao Donato <joaovictor_gd@hotmail.com>
2024-06-17 22:08:47 -0300

    first commit

---
+++ src/test/java/com/br/bank/api_bank/ApiBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.bank.api_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
